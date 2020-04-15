b2b1ba9f435c61db2d59e2df7a8a4819505ab6aa
ELEFTHERIOU Dimitrios <eledim@intrasoft-intl.com>
2020-04-16 00:21:15 +0200

    snippet plugin

--- src/standardword/standardword.js
+++ src/standardword/standardword.js
@@ -1,10 +1,10 @@
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import PlaceholderEditing from './standardwordediting';
+import StandardWordEditing from './standardwordediting';
 
 export default class StandardWord extends Plugin {
 	static get requires() {
-		return [PlaceholderEditing];
+		return [StandardWordEditing];
 	}
 
 	static get pluginName() {
